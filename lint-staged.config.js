13d98e3d7754446cbb979e0a52e4dc7ab9e0334f
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-04-15 18:57:44 +0100

    Add nodejs project config files
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

---
+++ lint-staged.config.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  "*.{js,jsx,ts,tsx,mjs}": "xo",
+  "*.{ts,tsx}": () => "tsc -p tsconfig.json --noEmit",
+  "*.md": "prettier --write",
+  "package.json": "prettier --write",
+};

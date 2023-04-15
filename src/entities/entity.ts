07f277e3dde77109aa6968ea2cfe27443a65dd6e
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-04-15 23:12:05 +0100

    Add basic player with basic movement
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

---
+++ src/entities/entity.ts
@@ -0,0 +1,21 @@
+abstract class Entity {
+  position: { x: number; y: number } = { x: 0, y: 0 };
+
+  init() {
+    //
+  }
+
+  update(dt: number) {
+    //
+  }
+
+  fixedUpdate(dt: number) {
+    //
+  }
+
+  render() {
+    //
+  }
+}
+
+export default Entity;

e59818b8e7553f94e22205b5ae41c55748db3943
m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>
2023-04-20 21:21:15 +0100

    Allow entity to accept the scene
    Signed-off-by: m4rc3l05 <15786310+M4RC3L05@users.noreply.github.com>

--- src/entities/entity.ts
+++ src/entities/entity.ts
@@ -1,7 +1,9 @@
 import { type Vector2 } from "raylib";
+import type Scene from "../scenes/scene.js";
 
 abstract class Entity {
   position: Vector2;
+  scene!: Scene;
 
   constructor(position?: Vector2) {
     this.position = position ?? { x: 0, y: 0 };
